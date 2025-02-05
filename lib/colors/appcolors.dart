65109883e0fce8bb1fbb78cfd8d9cdb78db4e24e
Sivappriya <yourofficeemail@example.com>
2025-02-05 15:12:01 +0530

    New Commit

--- lib/colors/appcolors.dart
+++ lib/colors/appcolors.dart
@@ -4,3 +4,4 @@ import 'package:flutter/material.dart';
 const Color primaryColor = Color(0xFF193482); // Blue primary color
 const Color white = Colors.white; // White text color
 const Color blackColor = Colors.black; // Black color
+const Color pink = Colors.pink; // Black color
